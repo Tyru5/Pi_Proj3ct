a228c4335bba3f3ff1de8689409b80c924060bed
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-05-16 15:27:02 -0600

    adding main

---
+++ smtp.py
@@ -0,0 +1,10 @@
+# Tyrus Malmstrom
+# May 16th,2016
+# Rasberry Pi Project
+
+import sys
+import smtplib #The smtplib module defines an SMTP client session object that can be used to send mail to any Internet machine with an SMTP or ESMTP listener daemon.
+
+
+def main():
+    print "Hello World!"

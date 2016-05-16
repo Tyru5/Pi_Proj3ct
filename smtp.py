426e06d7f4d619d05606457dec3dc27c31111baa
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-05-16 15:57:14 -0600

    testing out the email send

--- smtp.py
+++ smtp.py
@@ -10,6 +10,32 @@ from datetime import datetime
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
+# method that will allow the user to enter in details on the command line:
+def info(prompt):
+    return raw_input(prompt).strip()
 
-def main():
-    print "Hello World!"
+fromAddr = info("From: ")
+toAddr   = info("To: ").split()
+
+print "Now enter your message that you want to send to the person:"
+
+# Add the From: and To: headers at the start!
+current_msg = ("From: %s\r\nTo: %s\r\n\r\n" % (fromAddr, ", ".join(toAddr))) # string formatting.
+
+while True:
+    try:
+        line = raw_input()
+    except EOFError:
+        break
+    if not line:
+        break
+    current_msg = current_msg + line
+
+print "The length of your message is: ",len(current_msg),
+
+#now using the smptlib moduel to acutally send the email:
+smtpObj = smtplib.SMTP('localhost')
+smtpObj.set_debuglevel(1)
+smtpObj.sendmail(fromaddr, toaddrs, msg)
+smtpObj.quit()
+

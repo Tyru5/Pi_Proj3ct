1d7b2c00dcfb21f1e87ff5ab66112e44682716c7
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-05-16 16:44:44 -0600

    finalized

--- smtp.py
+++ smtp.py
@@ -2,7 +2,6 @@
 # May 16th,2016
 # Rasberry Pi Project
 
-import sys
 import smtplib #The smtplib module defines an SMTP client session object that can be used to send mail to any Internet machine with an SMTP or ESMTP listener daemon.
 # Import the email modules we'll need
 import time
@@ -14,28 +13,25 @@ from email.mime.multipart import MIMEMultipart
 def info(prompt):
     return raw_input(prompt).strip()
 
-fromAddr = info("From: ")
-toAddr   = info("To: ").split()
-
+email       = info("Email: ")
+recipient   = info("To: ")
+passW       =  info("Password: ")
 print "Now enter your message that you want to send to the person:"
 
 # Add the From: and To: headers at the start!
-current_msg = ("From: %s\r\nTo: %s\r\n\r\n" % (fromAddr, ", ".join(toAddr))) # string formatting.
+current_msg = info("Message: ")
 
-while True:
-    try:
-        line = raw_input()
-    except EOFError:
-        break
-    if not line:
-        break
-    current_msg = current_msg + line
-
-print "The length of your message is: ",len(current_msg),
+print "The length of your message is: ",len(current_msg)
 
 #now using the smptlib moduel to acutally send the email:
-smtpObj = smtplib.SMTP('localhost')
+smtpObj = smtplib.SMTP('smtp.gmail.com:587') # using gmail server to send the email.
+#testing the connection:
+smtpObj.ehlo()
+#secure connection:
+smtpObj.starttls()
+smtpObj.login( email, passW )
 smtpObj.set_debuglevel(1)
-smtpObj.sendmail(fromaddr, toaddrs, msg)
+smtpObj.sendmail( email, recipient, 'Subject: Info.\n' + current_msg )
+#disconnecting from smtp server:
 smtpObj.quit()
 

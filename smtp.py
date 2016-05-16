5890299247350e38f9252991f9834971e767fb58
Tyrus Malmstrom <tyrusm@hotmail.com>
2016-05-16 15:37:26 -0600

    finished adding all imports

--- smtp.py
+++ smtp.py
@@ -4,6 +4,11 @@
 
 import sys
 import smtplib #The smtplib module defines an SMTP client session object that can be used to send mail to any Internet machine with an SMTP or ESMTP listener daemon.
+# Import the email modules we'll need
+import time
+from datetime import datetime
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
 
 def main():
